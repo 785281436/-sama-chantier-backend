3122e1721b59ed556bb1ec8b134998c94a4ca25a
Abdoulaye Kande <abdoulaye.kande2@unchk.edu.sn>
2026-04-13 14:37:47 +0000

    fix: use @fluidjs/multer-cloudinary instead of multer-storage-cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,6 +1,6 @@
 const cloudinary = require('cloudinary').v2
 const multer = require('multer')
-const { CloudinaryStorage } = require('multer-storage-cloudinary')
+const { CloudinaryStorage } = require('@fluidjs/multer-cloudinary')
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
